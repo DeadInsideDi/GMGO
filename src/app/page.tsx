85b1baf601bb542f47ceeb3c1c1f14e6486d6293
DeadInsideDi <matvei778990@gmail.com>
2025-04-24 01:03:48 +0300

    headline states

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,24 @@
 'use client'
+import { usePathname } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import { ArrowRight } from '../../public'
 import { Carousel } from '../components/carousel/Carousel'
 import { Categories } from '../components/category/Categories'
+import { menuSections } from '../components/menu-bar/menu.data'
 import { NewsFeed } from '../components/news-feed/NewsFeed'
+import { useAccountStore } from '../store/account.store'
 import { useAppStore } from '../store/app.store'
 import { useCarouselStore } from '../store/carousel.store'
 import { useCategoryStore } from '../store/category.store'
 import './page.scss'
 
 export default function Page() {
+  const pathname = usePathname()
+  const sections = menuSections.find(menuItem => menuItem.href === pathname)
   const { currentCarouselData } = useCarouselStore()
   const { selectTopCategories } = useCategoryStore()
   const { isMobile } = useAppStore()
+  const { isLoggedIn } = useAccountStore()
 
   // HydrationWarning
   const [isClient, setIsClient] = useState(false)
@@ -25,7 +31,16 @@ export default function Page() {
       {isMobile ? (
         <>
           <h3>
-            Лента <ArrowRight />
+            {isLoggedIn() ? (
+              <>
+                {sections?.image}
+                {sections?.title}
+              </>
+            ) : (
+              <>
+                {sections?.title} <ArrowRight alt='arrow-right' />
+              </>
+            )}
           </h3>
           <Categories categories={selectTopCategories(5)} />
           <Carousel carouselData={currentCarouselData} />
