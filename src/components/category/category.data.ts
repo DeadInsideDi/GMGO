1381992157d31ddf8d84db07a4e41e44aa9ecbfc
DeadInsideDi <matvei778990@gmail.com>
2025-05-24 00:11:04 +0300

    review

--- src/components/category/category.data.ts
+++ src/components/category/category.data.ts
@@ -1,95 +1,95 @@
 export type TCategoryItem = {
-  id: number
-  name: string
-  image: string
-  href: string
-  usageCount: number
+	id: number
+	name: string
+	image: string
+	href: string
+	usageCount: number
 }
 
 export const generalCategory: TCategoryItem = {
-  id: 0,
-  name: 'Все',
-  image: '',
-  href: '/',
-  usageCount: 0,
+	id: 0,
+	name: 'Все',
+	image: '',
+	href: '/',
+	usageCount: 0,
 }
 
 export const categories = [
-  {
-    id: 1,
-    name: 'Пранки',
-    image: '',
-    href: '/category/pranks',
-    usageCount: 5,
-  },
-  {
-    id: 2,
-    name: 'Право',
-    image: '',
-    href: '/category/rights',
-    usageCount: 3,
-  },
-  {
-    id: 3,
-    name: 'Истории',
-    image: '',
-    href: '/category/history',
-    usageCount: 10,
-  },
-  {
-    id: 4,
-    name: 'Дизайн',
-    image: '/magic-fill-1.svg',
-    href: '/category/disign',
-    usageCount: 385,
-  },
-  {
-    id: 5,
-    name: 'Авто',
-    image: '/steering-wheel-1.svg',
-    href: '/category/auto',
-    usageCount: 45,
-  },
-  {
-    id: 6,
-    name: 'Искусство',
-    image: '/palette-fill-1.svg',
-    href: '/category/art',
-    usageCount: 12,
-  },
-  {
-    id: 7,
-    name: 'Бизнес',
-    image: '/briefcase-fill-1.svg',
-    href: '/category/business',
-    usageCount: 173,
-  },
-  {
-    id: 8,
-    name: 'Figma',
-    image: '',
-    href: '/category/figma',
-    usageCount: 293,
-  },
-  {
-    id: 9,
-    name: 'Инвестиции',
-    image: '',
-    href: '/category/investing',
-    usageCount: 248,
-  },
-  {
-    id: 10,
-    name: 'Лайфхаки',
-    image: '',
-    href: '/category/lifehacks',
-    usageCount: 118,
-  },
-  {
-    id: 11,
-    name: 'Эскизы',
-    image: '',
-    href: '/category/sketches',
-    usageCount: 23,
-  },
+	{
+		id: 1,
+		name: 'Пранки',
+		image: '',
+		href: '/category/pranks',
+		usageCount: 5,
+	},
+	{
+		id: 2,
+		name: 'Право',
+		image: '',
+		href: '/category/rights',
+		usageCount: 3,
+	},
+	{
+		id: 3,
+		name: 'Истории',
+		image: '',
+		href: '/category/history',
+		usageCount: 10,
+	},
+	{
+		id: 4,
+		name: 'Дизайн',
+		image: '/magic-fill-1.svg',
+		href: '/category/disign',
+		usageCount: 385,
+	},
+	{
+		id: 5,
+		name: 'Авто',
+		image: '/steering-wheel-1.svg',
+		href: '/category/auto',
+		usageCount: 745,
+	},
+	{
+		id: 6,
+		name: 'Искусство',
+		image: '/palette-fill-1.svg',
+		href: '/category/art',
+		usageCount: 1211,
+	},
+	{
+		id: 7,
+		name: 'Бизнес',
+		image: '/briefcase-fill-1.svg',
+		href: '/category/business',
+		usageCount: 1073,
+	},
+	{
+		id: 8,
+		name: 'Figma',
+		image: '',
+		href: '/category/figma',
+		usageCount: 293,
+	},
+	{
+		id: 9,
+		name: 'Инвестиции',
+		image: '',
+		href: '/category/investing',
+		usageCount: 248,
+	},
+	{
+		id: 10,
+		name: 'Лайфхаки',
+		image: '',
+		href: '/category/lifehacks',
+		usageCount: 118,
+	},
+	{
+		id: 11,
+		name: 'Эскизы',
+		image: '',
+		href: '/category/sketches',
+		usageCount: 23,
+	},
 ]
--- src/components/menu-bar/MenuBar.tsx
+++ src/components/menu-bar/MenuBar.tsx
@@ -11,75 +11,75 @@ import './MenuBar.scss'
 import { menuSections } from './menu.data'
 
 export const PublishButton: FC = () => {
-  const { isMobile } = useAppStore()
-  return (
-    <button
-      type='button'
-      aria-label='publish'
-      className='publish'>
-      <Image
-        src='/edit.svg'
-        alt='publish'
-        width={14}
-        height={14}
-      />
-      {!isMobile && 'Опубликовать'}
-    </button>
-  )
+	const { isMobile } = useAppStore()
+	return (
+		<button
+			type='button'
+			aria-label='publish'
+			className='publish'>
+			<Image
+				src='/edit.svg'
+				alt='publish'
+				width={14}
+				height={14}
+			/>
+			{!isMobile && 'Опубликовать'}
+		</button>
+	)
 }
 
 export const MenuBar: FC = () => {
-  const { burgerState, isMobile } = useAppStore()
-  const {
-    account: {
-      privacyInfo: { unreadMessages },
-    },
-  } = useAccountStore()
-  // HydrationWarning
-  const [isClient, setIsClient] = useState(false)
-  useEffect(() => setIsClient(true), [])
-  if (!isClient) return null
-  // HydrationWarning
-  return isMobile ? (
-    <div className='menu-bar-mobile'>
-      {menuSections.slice(1, 3).map(menuItem => (
-        <Section
-          key={menuItem.id}
-          menuItem={{ ...menuItem, title: '' }}
-        />
-      ))}
-      <Link
-        aria-label='Go to messages'
-        href={'/messages'} // not existing route
-        className='messages'>
-        <Envelope
-          alt='messages'
-          width={24}
-          height={24}
-        />
-        {unreadMessages ? <span className='unread-messages'>{unreadMessages}</span> : ''}
-      </Link>
-      {menuSections.slice(3, 5).map(menuItem => (
-        <Section
-          key={menuItem.id}
-          menuItem={{ ...menuItem, title: '' }}
-        />
-      ))}
-    </div>
-  ) : (
-    <div
-      className='menu-bar'
-      aria-disabled={!burgerState}>
-      <PublishButton />
-      <div className='sections'>
-        {menuSections.map(menuItem => (
-          <Section
-            key={menuItem.id}
-            menuItem={menuItem}
-          />
-        ))}
-      </div>
-      <Footer />
-    </div>
-  )
+	const { burgerState, isMobile } = useAppStore()
+	const {
+		account: {
+			privacyInfo: { unreadMessages },
+		},
+	} = useAccountStore()
+	// HydrationWarning
+	const [isClient, setIsClient] = useState(false)
+	useEffect(() => setIsClient(true), [])
+	if (!isClient) return null
+	// HydrationWarning
+	return isMobile ? (
+		<div className='menu-bar-mobile'>
+			{menuSections.slice(1, 3).map(menuItem => (
+				<Section
+					key={menuItem.id}
+					menuItem={{ ...menuItem, title: '' }}
+				/>
+			))}
+			<Link
+				aria-label='Go to messages'
+				href={'/messages'} // not designed route
+				className='messages'>
+				<Envelope
+					alt='messages'
+					width={24}
+					height={24}
+				/>
+				{unreadMessages ? <span className='unread-messages'>{unreadMessages}</span> : ''}
+			</Link>
+			{menuSections.slice(3, 5).map(menuItem => (
+				<Section
+					key={menuItem.id}
+					menuItem={{ ...menuItem, title: '' }}
+				/>
+			))}
+		</div>
+	) : (
+		<div
+			className='menu-bar'
+			aria-disabled={!burgerState}>
+			<PublishButton />
+			<div className='sections'>
+				{menuSections.map(menuItem => (
+					<Section
+						key={menuItem.id}
+						menuItem={menuItem}
+					/>
+				))}
+			</div>
+			<Footer />
+		</div>
+	)
 }
